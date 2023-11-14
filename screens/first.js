07d686b6a97953e156d5ce7916b69a520a858462
viet <viet1282002.com>
2023-11-14 22:50:03 +0700

    2

---
+++ screens/first.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+
+export default function first(){
+    return(
+        <View>
+            <Text>hello</Text>
+        </View>
+    )
+}
