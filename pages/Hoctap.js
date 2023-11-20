caad0bec9850bd17478795a60607b8ee95988944
viet <viet1282002.com>
2023-11-20 15:15:27 +0700

    update

--- pages/Hoctap.js
+++ pages/Hoctap.js
@@ -1,10 +1,34 @@
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Pressable } from 'react-native';
 
 
 export default function SecondPage(){
     return(
-        <View>
-            <Text>Text</Text>
+        <View style={styles.container}>
+            <Pressable style={styles.btn}>BẢNG XẾP HẠNG</Pressable>
+            <Pressable style={styles.btn}>THI TRỰC TUYẾN</Pressable>
+            <Pressable style={styles.btn}>OLYMPIC TIẾNG ANH</Pressable>
+            <Pressable style={styles.btn}>BIÊN CƯƠNG TỔ QUỐC TÔI</Pressable>
+            <Pressable style={styles.btn}>TỰ HÀO VIỆT NAM</Pressable>
+            
         </View>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    //   justifyContent: 'center',
+      alignItems :'center',
+      
+     
+    },
+    btn:{
+        width: "80%",
+        height:50,
+        borderWidth:1,
+        borderRadius : 20,
+        justifyContent : 'center',
+        alignItems :'center',
+        marginTop:20,
+        fontWeight :'bold'
+    }
+})
