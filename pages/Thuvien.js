771b1950af9d022330b713c54bc6d14e2e5be418
viet <viet1282002.com>
2023-11-20 13:04:49 +0700

    tab

---
+++ pages/Thuvien.js
@@ -0,0 +1,10 @@
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+
+export default function Thuvien(){
+    return(
+        <View>
+            <Text>Text</Text>
+        </View>
+    )
+}
---
+++ pages/Tintuc.js
@@ -0,0 +1,10 @@
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+
+export default function FirstPage(){
+    return(
+        <View>
+            <Text>Text</Text>
+        </View>
+    )
+}
