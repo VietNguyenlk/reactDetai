48522c43175e67a7746506acc4ce80ae3f5bac1d
viet <viet1282002.com>
2023-11-24 19:32:49 +0700

    update

--- pages/call.js
+++ pages/call.js
@@ -11,31 +11,31 @@ import Giaitri from '../pages/Giaitri';
 import Audiobook from '../pages/Audiobook';
 import Thuvien from '../pages/Thuvien';
 
-export default function Call(){
+export default function Call({navigation}){
    
     return(
         <View style={styles.container}>
-            <View style={{backgroundColor:'blue',height:60,display:'flex',flexDirection:'row'}}>
+            <View style={{backgroundColor:'#0066FF',height:60,display:'flex',flexDirection:'row'}}>
                 <Image style={{width:30,height:30,marginTop:13,marginLeft:10}} source={require('../assets/acc.png')}/>
                 <Text style={{fontSize:20,color:'white',marginTop:13,marginLeft:10}}>Trương Tuấn Kiệt</Text>
                 <Image style={{width:30,height:30,marginTop:13,marginLeft:100}} source={require('../assets/kinh.png')}/>
                 <Image style={{width:30,height:30,marginTop:13,marginLeft:10}} source={require('../assets/chuong.png')}/>
             </View>
-            <View style={{marginTop:20, height:50,width:'100%',display:'flex',flexDirection:"row",justifyContent:"space-around"}}>
-                <TouchableOpacity>
-                    <Text style={{color:'gray'}}>Tin tức</Text>
+            <View style={{alignItems:'center',backgroundColor:'gray', height:60,width:'100%',display:'flex',flexDirection:"row",justifyContent:"space-around"}}>
+                <TouchableOpacity onPress={()=>navigation.navigate('first')}>
+                    <Text style={{color:'white'}}>Tin tức</Text>
                 </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text style={{color:'gray'}}>Học tập</Text>
+                <TouchableOpacity onPress={()=>navigation.navigate('first')} >
+                    <Text style={{color:'white'}}>Học tập</Text>
                 </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text style={{color:'gray'}}>Giải trí</Text>
+                <TouchableOpacity onPress={()=>navigation.navigate('first')}>
+                    <Text style={{color:'white'}}>Giải trí</Text>
                 </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text style={{color:'gray'}}>Đại hội</Text>
+                <TouchableOpacity onPress={()=>navigation.navigate('first')}>
+                    <Text style={{color:'white'}}>Đại hội</Text>
                 </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text style={{color:'gray'}}>Thư viện</Text>
+                <TouchableOpacity onPress={()=>navigation.navigate('first')}>
+                    <Text style={{color:'white'}}>Thư viện</Text>
                 </TouchableOpacity>
             </View>
             <ScrollView style={{height:100}}>
@@ -46,23 +46,23 @@ export default function Call(){
             </ScrollView>
             <Image style={{marginLeft:"80%",width:50,height:50}} source={require('../assets/logocall.png')}/>
             <View style={{display:'flex',flexDirection:'row',justifyContent: 'space-between',marginTop:10}}> 
-              <TouchableOpacity style={{marginLeft:10,alignItems:'center'}}>
+              <TouchableOpacity onPress={()=>navigation.navigate('Trangchu')} style={{marginLeft:10,alignItems:'center'}}>
                 <Image style={{width:25,height:25,}} source={require('../assets/vector2.png')}/>
                 <Text>Trang chủ</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={{marginRight:0,marginLeft:0,alignItems:'center'}}>
+              <TouchableOpacity onPress={()=>navigation.navigate('Chat')} style={{marginRight:0,marginLeft:0,alignItems:'center'}}>
                 <Image style={{width:25,height:25}} source={require('../assets/chat 1.png')}/>
                 <Text>Chat</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={{marginRight:0,marginLeft:0,alignItems:'center'}}>
+              <TouchableOpacity  style={{marginRight:0,marginLeft:0,alignItems:'center'}}>
                 <Image style={{width:25,height:25}} source={require('../assets/dienthoai2.png')}/>
                 <Text>Cuộc gọi</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={{marginRight:0,marginLeft:0,alignItems:'center'}}> 
+              <TouchableOpacity onPress={()=>navigation.navigate('Danhba')} style={{marginRight:0,marginLeft:0,alignItems:'center'}}> 
                 <Image style={{width:25,height:25}} source={require('../assets/nguoi.png')}/>
                 <Text>Danh bạ</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={{marginRight:10,alignItems:'center'}}>
+              <TouchableOpacity onPress={()=>navigation.navigate('Khac')} style={{marginRight:10,alignItems:'center'}}>
                 <Image style={{width:25,height:25}} source={require('../assets/apps-alt 1.png')}/>
                 <Text>Khác</Text>
               </TouchableOpacity>
--- pages/chat.js
+++ pages/chat.js
@@ -15,27 +15,27 @@ export default function Chat({navigation}){
    
     return(
         <View style={styles.container}>
-            <View style={{backgroundColor:'blue',height:60,display:'flex',flexDirection:'row'}}>
+            <View style={{backgroundColor:'#0066FF',height:60,display:'flex',flexDirection:'row'}}>
                 <Image style={{width:30,height:30,marginTop:13,marginLeft:10}} source={require('../assets/acc.png')}/>
                 <Text style={{fontSize:20,color:'white',marginTop:13,marginLeft:10}}>Trương Tuấn Kiệt</Text>
                 <Image style={{width:30,height:30,marginTop:13,marginLeft:100}} source={require('../assets/kinh.png')}/>
                 <Image style={{width:30,height:30,marginTop:13,marginLeft:10}} source={require('../assets/chuong.png')}/>
             </View>
-            <View style={{marginTop:20, height:50,width:'100%',display:'flex',flexDirection:"row",justifyContent:"space-around"}}>
-                <TouchableOpacity>
-                    <Text style={{color:'gray'}}>Tin tức</Text>
+            <View style={{ backgroundColor:'gray',alignItems:'center',height:60,width:'100%',display:'flex',flexDirection:"row",justifyContent:"space-around"}}>
+                <TouchableOpacity onPress={()=>navigation.navigate('first')}>
+                    <Text style={{color:'white'}}>Tin tức</Text>
                 </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text style={{color:'gray'}}>Học tập</Text>
+                <TouchableOpacity onPress={()=>navigation.navigate('first')}>
+                    <Text style={{color:'white'}}>Học tập</Text>
                 </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text style={{color:'gray'}}>Giải trí</Text>
+                <TouchableOpacity onPress={()=>navigation.navigate('first')}>
+                    <Text style={{color:'white'}}>Giải trí</Text>
                 </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text style={{color:'gray'}}>Đại hội</Text>
+                <TouchableOpacity onPress={()=>navigation.navigate('first')}>
+                    <Text style={{color:'white'}}>Đại hội</Text>
                 </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text style={{color:'gray'}}>Thư viện</Text>
+                <TouchableOpacity onPress={()=>navigation.navigate('first')}>
+                    <Text style={{color:'white'}}>Thư viện</Text>
                 </TouchableOpacity>
             </View>
             <ScrollView style={{height:100}}>
@@ -54,15 +54,15 @@ export default function Chat({navigation}){
                 <Image style={{width:25,height:25}} source={require('../assets/chat 2.png')}/>
                 <Text>Chat</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={{marginRight:0,marginLeft:0,alignItems:'center'}}>
+              <TouchableOpacity onPress={()=>navigation.navigate('Call')} style={{marginRight:0,marginLeft:0,alignItems:'center'}}>
                 <Image style={{width:25,height:25}} source={require('../assets/dienthoai.png')}/>
                 <Text>Cuộc gọi</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={{marginRight:0,marginLeft:0,alignItems:'center'}}> 
+              <TouchableOpacity onPress={()=>navigation.navigate('Danhba')}style={{marginRight:0,marginLeft:0,alignItems:'center'}}> 
                 <Image style={{width:25,height:25}} source={require('../assets/nguoi.png')}/>
                 <Text>Danh bạ</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={{marginRight:10,alignItems:'center'}}>
+              <TouchableOpacity onPress={()=>navigation.navigate('Khac')} style={{marginRight:10,alignItems:'center'}}>
                 <Image style={{width:25,height:25}} source={require('../assets/apps-alt 1.png')}/>
                 <Text>Khác</Text>
               </TouchableOpacity>
